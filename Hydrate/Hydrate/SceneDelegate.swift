f168703bd446733675b62799977bd519b0b89aea
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-17 06:43:55 -0400

    Refactored ViewController to MainViewController and added Extension+UIView to handle layout constraints

--- Hydrate/Hydrate/SceneDelegate.swift
+++ Hydrate/Hydrate/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
 
--- Hydrate/Hydrate/View Controllers/MainViewController.swift
+++ Hydrate/Hydrate/View Controllers/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  Hydrate
 //
 //  Created by Joshua Rutkowski on 6/16/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
