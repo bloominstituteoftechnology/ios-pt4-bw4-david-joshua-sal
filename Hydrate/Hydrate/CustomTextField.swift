2271d5102b9b94d0cc65ebd4f4a1902dc1c1a3cf
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-17 21:14:05 -0400

    Setup popupView

---
+++ Hydrate/Hydrate/CustomTextField.swift
@@ -0,0 +1,26 @@
+//
+//  CustomTextField.swift
+//  Hydrate
+//
+//  Created by Joshua Rutkowski on 6/17/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    let padding = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+
+    override open func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+
+    override open func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.inset(by: padding)
+    }
+}
