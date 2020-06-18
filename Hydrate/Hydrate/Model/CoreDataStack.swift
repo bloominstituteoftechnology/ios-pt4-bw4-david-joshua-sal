03d8ac065b2cbe28ee45736f3e03e49ec7d4f890
David Wright <davidtwright89@gmail.com>
2020-06-17 21:00:50 -0400

    Created the CoreDataStack

---
+++ Hydrate/Hydrate/Model/CoreDataStack.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataStack.swift
+//  Hydrate
+//
+//  Created by David Wright on 6/17/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    private init() {}
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    private lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Hydrate")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() throws {
+        guard mainContext.hasChanges else { return }
+        
+        var error: Error?
+        
+        mainContext.performAndWait {
+            do {
+                try mainContext.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error { throw error }
+    }
+}
