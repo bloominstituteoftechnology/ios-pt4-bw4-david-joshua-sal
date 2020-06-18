dd8099997c7f647b888d9e1b11033de11aedb080
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-18 08:45:12 -0400

    Created WaveAnimationView class

---
+++ Hydrate/Hydrate/Views/WaveAnimationView.swift
@@ -0,0 +1,21 @@
+//
+//  WaveAnimationView.swift
+//  Hydrate
+//
+//  Created by Joshua Rutkowski on 6/18/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import UIKit
+
+class WaveAnimationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
