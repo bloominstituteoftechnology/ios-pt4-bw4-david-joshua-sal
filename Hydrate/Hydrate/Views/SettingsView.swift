30b2d32dd8098197afdef0d5ff60622820170e05
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-23 08:07:28 -0400

    Setting up blank settings view in SwiftUI

---
+++ Hydrate/Hydrate/Views/SettingsView.swift
@@ -0,0 +1,22 @@
+//
+//  SettingsView.swift
+//  Hydrate
+//
+//  Created by Joshua Rutkowski on 6/23/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    
+    var body: some View {
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
