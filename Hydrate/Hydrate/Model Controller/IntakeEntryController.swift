689e0a564b1533dbd056a2a5bdafdffaaf0c64ce
David Wright <davidtwright89@gmail.com>
2020-06-23 16:39:19 -0400

    Refactored the IntakeEntryController model controller

--- Hydrate/Hydrate/Model Controller/IntakeEntryController.swift
+++ Hydrate/Hydrate/Model Controller/IntakeEntryController.swift
@@ -11,37 +11,66 @@ import CoreData
 
 class IntakeEntryController {
     
-    // MARK: - Properties
+    // MARK: - Public Properties
     
     var intakeEntries: [IntakeEntry] = []
-    
-    var allIntakeEntries: [IntakeEntry] = []
-    var daysWithIntakeEntries: [Date] = []
-    var dailyTotals: [Int] = []
-    
-    var coreDataStack = CoreDataStack.shared
     
     var totalIntakeAmount: Int {
         Int(intakeEntries.reduce(0) { $0 + $1.intakeAmount })
     }
     
-    var allIntakeEntriesFetchRequest: NSFetchRequest<IntakeEntry> = {
+    init() {
+        fetchIntakeEntries()
+    }
+    
+    // MARK: - Private Properties
+    
+    fileprivate var coreDataStack = CoreDataStack.shared
+    
+    // MARK: - Public CRUD Methods
+    
+    func fetchIntakeEntries(for date: Date = Date()) {
         let fetchRequest: NSFetchRequest<IntakeEntry> = IntakeEntry.fetchRequest()
-        let timeSortDescriptor = NSSortDescriptor(key: #keyPath(IntakeEntry.timestamp), ascending: false)
-        fetchRequest.sortDescriptors = [timeSortDescriptor]
-        return fetchRequest
-    }()
-    
-    func getAllDatesWithIntakeEntries() -> [Date] {
-        let allDates = allIntakeEntries.compactMap { $0.timestamp?.startOfDay }
-        return Array(Set(allDates)).sorted()
+        let datePredicate = NSPredicate(format: "(%K >= %@) AND (%K < %@)",
+                                        #keyPath(IntakeEntry.timestamp), date.startOfDay as NSDate,
+                                        #keyPath(IntakeEntry.timestamp), date.startOfNextDay as NSDate)
+        fetchRequest.predicate = datePredicate
+        
+        do {
+            let intakeEntries = try coreDataStack.mainContext.fetch(fetchRequest)
+            self.intakeEntries = intakeEntries
+        } catch let error as NSError {
+            print("Error fetching: \(error), \(error.userInfo)")
+            self.intakeEntries = []
+        }
     }
     
-    func getAllIntakeTotals() -> [Int] {
-        return daysWithIntakeEntries.compactMap { fetchIntakeTotal(for: $0) }
+    func addIntakeEntry(withIntakeAmount intakeAmount: Int) {
+        let intakeEntry = IntakeEntry(intakeAmount: intakeAmount)
+        
+        do {
+            try CoreDataStack.shared.saveContext()
+            intakeEntries.insert(intakeEntry, at: 0)
+        } catch {
+            print("Error adding intakeEntry: \(error)")
+        }
     }
     
-    func fetchIntakeTotal(for date: Date = Date()) -> Int {
+    func deleteIntakeEntry(_ intakeEntry: IntakeEntry) {
+        let context = CoreDataStack.shared.mainContext
+        do {
+            context.delete(intakeEntry)
+            try context.save()
+            intakeEntries.removeAll(where: { $0 == intakeEntry })
+        } catch {
+            print("Error deleting intakeEntry: \(error)")
+        }
+    }
+    
+    // MARK: - Private Methods
+    
+    /*// This Core Data retrieval function might be used later on. Delete if unused.
+    fileprivate func fetchIntakeTotal(for date: Date = Date()) -> Int {
         let fetchRequest = NSFetchRequest<NSDictionary>(entityName: "IntakeEntry")
         fetchRequest.resultType = .dictionaryResultType
         
@@ -69,48 +98,5 @@ class IntakeEntryController {
             return 0
         }
     }
-    
-    // MARK: - CRUD Methods
-    
-    func loadIntakeEntries(for date: Date = Date()) {
-        self.intakeEntries = fetchIntakeEntries(for: date)
-    }
-    
-    func fetchIntakeEntries(for date: Date = Date()) -> [IntakeEntry] {
-        let fetchRequest: NSFetchRequest<IntakeEntry> = IntakeEntry.fetchRequest()
-        let datePredicate = NSPredicate(format: "(%K >= %@) AND (%K < %@)",
-                                        #keyPath(IntakeEntry.timestamp), date.startOfDay as NSDate,
-                                        #keyPath(IntakeEntry.timestamp), date.startOfNextDay as NSDate)
-        fetchRequest.predicate = datePredicate
-        
-        do {
-            let intakeEntries = try coreDataStack.mainContext.fetch(fetchRequest)
-            return intakeEntries
-        } catch let error as NSError {
-            print("Error fetching: \(error), \(error.userInfo)")
-            return []
-        }
-    }
-    
-    func addIntakeEntry(withIntakeAmount intakeAmount: Int) {
-        let intakeEntry = IntakeEntry(intakeAmount: intakeAmount)
-        
-        do {
-            try CoreDataStack.shared.saveContext()
-            intakeEntries.insert(intakeEntry, at: 0)
-        } catch {
-            print("Error adding intakeEntry: \(error)")
-        }
-    }
-    
-    func deleteIntakeEntry(_ intakeEntry: IntakeEntry) {
-        let context = CoreDataStack.shared.mainContext
-        do {
-            context.delete(intakeEntry)
-            try context.save()
-            intakeEntries.removeAll(where: { $0 == intakeEntry })
-        } catch {
-            print("Error deleting intakeEntry: \(error)")
-        }
-    }
+    */
 }
