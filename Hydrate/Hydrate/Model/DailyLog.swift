2562a07a08d1c9643d994e651157849eec712371
David Wright <davidtwright89@gmail.com>
2020-06-23 16:35:46 -0400

    Implemented the DailyLog model object

--- Hydrate/Hydrate/Model/DailyLog.swift
+++ Hydrate/Hydrate/Model/DailyLog.swift
@@ -11,4 +11,37 @@ import CoreData
 
 class DailyLog {
     
+    // Properties
+    var date: Date
+    var entries: [IntakeEntry]
+    
+    var totalIntakeAmount: Int {
+        Int(entries.reduce(0) { $0 + $1.intakeAmount })
+    }
+    
+    // Initializers
+    init(date: Date = Date(), entries: [IntakeEntry]) {
+        self.date = date.startOfDay
+        self.entries = entries
+    }
+    
+    /*// This conveniece initializer might be used later on. Delete if unused.
+    init(date: Date = Date()) {
+        self.date = date.startOfDay
+
+        let fetchRequest: NSFetchRequest<IntakeEntry> = IntakeEntry.fetchRequest()
+        let datePredicate = NSPredicate(format: "(%K >= %@) AND (%K < %@)",
+                                        #keyPath(IntakeEntry.timestamp), date.startOfDay as NSDate,
+                                        #keyPath(IntakeEntry.timestamp), date.startOfNextDay as NSDate)
+        fetchRequest.predicate = datePredicate
+
+        do {
+            let intakeEntries = try CoreDataStack.shared.mainContext.fetch(fetchRequest)
+            self.entries = intakeEntries
+        } catch let error as NSError {
+            print("Error fetching: \(error), \(error.userInfo)")
+            self.entries = []
+        }
+    }
+    */
 }
