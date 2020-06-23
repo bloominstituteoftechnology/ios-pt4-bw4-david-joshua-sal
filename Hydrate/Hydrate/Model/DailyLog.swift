8e28c1c2925d6bdc772533ea2ba502701585e875
David Wright <davidtwright89@gmail.com>
2020-06-23 16:33:25 -0400

    Added a DailyLog class which will be used in the HistoryTableViewController's data source

---
+++ Hydrate/Hydrate/Model/DailyLog.swift
@@ -0,0 +1,14 @@
+//
+//  DailyLog.swift
+//  Hydrate
+//
+//  Created by David Wright on 6/23/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class DailyLog {
+    
+}
