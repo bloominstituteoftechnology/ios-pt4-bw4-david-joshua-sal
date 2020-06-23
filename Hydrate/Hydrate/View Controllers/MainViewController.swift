dd03b1c6e46890730340ca9ef09dd33024a8e3e4
David Wright <davidtwright89@gmail.com>
2020-06-23 16:40:38 -0400

    Updated the MainViewController to reflect the refactored IntakeEntryController

--- Hydrate/Hydrate/View Controllers/MainViewController.swift
+++ Hydrate/Hydrate/View Controllers/MainViewController.swift
@@ -54,7 +54,7 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        intakeEntryController.loadIntakeEntries()
+        intakeEntryController.fetchIntakeEntries()
         
         wave = WaveAnimationView(frame: CGRect(origin: .zero, size: view.bounds.size),
                                  color: UIColor.sicklySmurfBlue.withAlphaComponent(0.5))
