9152d6a31e4f7cd493c067438831c5c06f16340c
David Wright <davidtwright89@gmail.com>
2020-06-23 02:57:30 -0400

    Implemented the CoreDataStack

--- Hydrate/Hydrate/Model/CoreDataStack.swift
+++ Hydrate/Hydrate/Model/CoreDataStack.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    private init() {}
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    private lazy var container: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "Hydrate")
+        container.loadPersistentStores { _, error in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+        return container
+    }()
+    
+    func saveContext() throws {
+        guard mainContext.hasChanges else { return }
+        
+        var error: Error?
+        
+        mainContext.performAndWait {
+            do {
+                try mainContext.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        
+        if let error = error { throw error }
+    }
+}
