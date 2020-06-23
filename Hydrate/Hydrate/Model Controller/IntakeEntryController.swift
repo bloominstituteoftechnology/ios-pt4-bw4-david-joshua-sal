84e77bd40db80381c7889edff8f1d506cbda7148
David Wright <davidtwright89@gmail.com>
2020-06-23 03:01:29 -0400

    Added a model controller named IntakeEntryController

---
+++ Hydrate/Hydrate/Model Controller/IntakeEntryController.swift
@@ -0,0 +1,13 @@
+//
+//  IntakeEntryController.swift
+//  Hydrate
+//
+//  Created by David Wright on 6/23/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import Foundation
+
+class IntakeEntryController {
+    
+}
