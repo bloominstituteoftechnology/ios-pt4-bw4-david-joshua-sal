1895f0a71398a12129e50e1457a4cd63163817b2
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-24 16:46:32 -0400

    Updated AppDelegate to conform to UNUserNotificationCenterDelegate. Notifications will also arrive while app is in foreground now.

--- Hydrate/Hydrate/Resources/AppDelegate.swift
+++ Hydrate/Hydrate/Resources/AppDelegate.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
 
-
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
+        -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
         return true
     }
 
