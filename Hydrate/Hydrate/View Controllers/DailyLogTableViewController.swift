1ac4e6081a6e6ae62bf8e63c402fa477deff5549
David Wright <davidtwright89@gmail.com>
2020-06-24 00:15:09 -0400

    Copied over the basic tableView functionality from the HistoryTableViewController into the new DailyLogTableViewController

--- Hydrate/Hydrate/View Controllers/DailyLogTableViewController.swift
+++ Hydrate/Hydrate/View Controllers/DailyLogTableViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class DailyLogTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    
+    let dailyLogController = DailyLogController()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,38 +24,44 @@ class DailyLogTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        title = "Daily Logs"
+        updateViews()
+    }
+    
+    // MARK: - Internal
+    
+    fileprivate var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter
+    }()
+    
+    fileprivate func updateViews() {
+        guard isViewLoaded else { return }
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        dailyLogController.dailyLogs.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: "dailyLogCell")
+        if cell == nil {
+            cell = UITableViewCell(style: .value1, reuseIdentifier: "dailyLogCell")
+        }
+        
+        let dailyLog = dailyLogController.dailyLogs[indexPath.row]
+        cell.textLabel?.text = dateFormatter.string(from: dailyLog.date)
+        cell.detailTextLabel?.text = "\(dailyLog.totalIntakeAmount) oz."
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -58,25 +70,10 @@ class DailyLogTableViewController: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     /*
     // MARK: - Navigation
 
@@ -86,5 +83,4 @@ class DailyLogTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
