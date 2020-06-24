7571636e9afb98341921248ee9bf21614db1f19b
Sal B Amer <iosdevsalman@gmail.com>
2020-06-24 05:27:03 -0700

    Added ProgressInterfaceController

---
+++ Hydrate/Hydrate-WatchOS Extension/ProgressInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  ProgressInterfaceController.swift
+//  Hydrate-WatchOS Extension
+//
+//  Created by Sal B Amer on 6/24/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ProgressInterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
