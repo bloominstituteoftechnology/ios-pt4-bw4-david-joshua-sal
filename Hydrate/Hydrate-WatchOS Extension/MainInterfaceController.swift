1e326b14b9dbe81c04bfecaf3693014a01e97644
Sal B Amer <iosdevsalman@gmail.com>
2020-06-25 00:28:16 -0700

    healthKit Store
    Added healthKit Store but unable to trigger from inside watchOS App

--- Hydrate/Hydrate-WatchOS Extension/MainInterfaceController.swift
+++ Hydrate/Hydrate-WatchOS Extension/MainInterfaceController.swift
@@ -19,7 +19,8 @@ class MainInterfaceController: WKInterfaceController {
     
     @IBOutlet weak var waterPercentageLabel: WKInterfaceLabel!
     
-//    let intakeEntryController = IntakeEntryController()
+//    let intakeEntryController = IntakeEntryController() -- unable to access coreData
+    let healthKitStore = HKHealthStore()
     
 
     override func awake(withContext context: Any?) {
@@ -31,6 +32,7 @@ class MainInterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        
     }
     
     override func didDeactivate() {
@@ -38,6 +40,52 @@ class MainInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
     
+    func authorizeHealthKit() {
+        HealthKitSetupHelper().authorize { (authorized, error) in
+            guard authorized else {
+                let baseMessage = "HealthKit Authorization Failed"
+                
+                if let error = error {
+                    print("\(baseMessage). Reason: \(error.localizedDescription)")
+                } else {
+                    print(baseMessage)
+                }
+                
+                return
+            }
+            print("HealthKit Successfully Authorized")
+            self.startObservingChanges()
+        }
+    }
+    
+    //observe changes to healthApp
+    func startObservingChanges() {
+        let query: HKObserverQuery = HKObserverQuery(sampleType: HealthKitWaterDataStore.waterType, predicate: nil, updateHandler: self.waterChangedHandler)
+                healthKitStore.execute(query)
+    }
+    //water level changed
+    func waterChangedHandler(query: HKObserverQuery, completionHandler: @escaping HKObserverQueryCompletionHandler, error: Error?) {
+            guard error == nil else {
+            print("failed trigger \(String(describing: error))")
+            return
+        }
+            self.readWater()
+            completionHandler()
+    }
+    //read water levels
+    private func readWater() {
+        HealthKitWaterDataStore().readWaterData(completion: {total in
+        print("total water: \(total)") //TODO Convert to Percentage
+        DispatchQueue.main.async {
+            self.waterPercentageLabel.setText("\(total) %") //currently in ML
+        }
+    })
+}
+    //write water levels back to Store - Not on this screen o
+    private func writeWater(amount: Double) {
+        HealthKitWaterDataStore().writeWaterData(amount: amount)
+    }
+
     //MARK: Actions
     //not needed direct connection to addDrinkInterfaceController
     
