73527f7cd74abdfa08a2c11082a9291d583f075b
Sal B Amer <iosdevsalman@gmail.com>
2020-06-25 00:28:16 -0700

    healthKit Store
    Added healthKit Store but unable to trigger from inside watchOS App

--- Hydrate/Hydrate-WatchOS Extension/AddDrinkInterfaceController.swift
+++ Hydrate/Hydrate-WatchOS Extension/AddDrinkInterfaceController.swift
@@ -8,10 +8,13 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
 
 class AddDrinkInterfaceController: WKInterfaceController {
 
+    let healthKitStore = HKHealthStore()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -27,13 +30,25 @@ class AddDrinkInterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    // Do i have to authorize again? No for now
+    
     //MARK: Actions
     
     @IBAction func addEightOzBtn() {
+        writeWater(amount: 240) // 8 oz = 240 ml
+        popToRootController()
     }
     @IBAction func addSixteenOzBtn() {
+        writeWater(amount: 473) // 16 oz = 473 oz
+        popToRootController()
     }
     // custom button not needed direct segue
+    
+    //Add Water to HK Store
+    private func writeWater(amount: Double) {
+        HealthKitWaterDataStore().writeWaterData(amount: amount)
+    }
     
     
 }
