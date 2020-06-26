f07df8cf940bae77dba4d3575fe0b276eedba322
David Wright <davidtwright89@gmail.com>
2020-06-26 09:20:12 -0400

    Implemented DailyLog in Objective-C

--- Hydrate/Hydrate/Models/DailyLog.m
+++ Hydrate/Hydrate/Models/DailyLog.m
@@ -10,4 +10,27 @@
 
 @implementation DailyLog
 
+- (instancetype)initWithDate:(NSDate *)date entries:(NSArray<IntakeEntry *> *)entries
+{
+    if (self = [super init]) {
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+        NSDateComponents *components = [calendar components:( NSCalendarUnitMonth | NSCalendarUnitYear |
+                                                             NSCalendarUnitHour | NSCalendarUnitMinute |
+                                                             NSCalendarUnitSecond ) fromDate:date];
+        [components setHour:0];
+        [components setMinute:0];
+        [components setSecond:0];
+
+        _date = [calendar dateFromComponents:components];
+        _entries = entries.copy;
+    }
+    return self;
+}
+
+- (NSInteger)totalIntakeAmount
+{
+    NSNumber *sum = [_entries.copy valueForKeyPath:@"@sum.intakeAmount"];
+    return sum.integerValue;
+}
+
 @end
