5d1bd5152ef5f076fb5fa5dbe743f4916549d4b7
David Wright <davidtwright89@gmail.com>
2020-06-26 08:37:12 -0400

    Added a DailyLog Objective-C file and created a bridging header

---
+++ Hydrate/Hydrate/Models/DailyLog.h
@@ -0,0 +1,17 @@
+//
+//  DailyLog.h
+//  Hydrate
+//
+//  Created by David Wright on 6/26/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DailyLog : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Hydrate/Hydrate/Models/DailyLog.m
@@ -0,0 +1,13 @@
+//
+//  DailyLog.m
+//  Hydrate
+//
+//  Created by David Wright on 6/26/20.
+//  Copyright © 2020 Hydrate. All rights reserved.
+//
+
+#import "DailyLog.h"
+
+@implementation DailyLog
+
+@end
