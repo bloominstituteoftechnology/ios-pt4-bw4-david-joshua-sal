3e5b0a5b29fc6e45165eb69d8354656d053a16f4
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-06-26 16:01:10 -0400

    Updated chartsView to log current data from CoreData

--- Hydrate/Hydrate/Views/ChartsView.swift
+++ Hydrate/Hydrate/Views/ChartsView.swift
@@ -5,15 +5,28 @@
 //  Created by Joshua Rutkowski on 6/26/20.
 //  Copyright © 2020 Hydrate. All rights reserved.
 //
-
 import SwiftUI
+import CoreData
 
 struct ChartsView: View {
     
-    // Dummy data
-    @State var dataPoints: [CGFloat] = [
-    10, 20, 30 , 40, 50, 60, 70
-    ]
+    let dailyLogController = DailyLogController()
+    var dailyLogs: [DailyLog] = []
+    var lastSevenDays: [DailyLog] = []
+    
+    var day1: CGFloat = 0
+    var day2: CGFloat = 0
+    var day3: CGFloat = 0
+    var day4: CGFloat = 0
+    var day5: CGFloat = 0
+    var day6: CGFloat = 0
+    var day7: CGFloat = 0
+    
+    init() {
+        updateDailyLogs()
+        
+        checkIfIndexExists()
+    }
     
     var body: some View {
         ZStack {
@@ -25,18 +38,18 @@ struct ChartsView: View {
                         .fontWeight(.heavy)
                         .foregroundColor(Color(UIColor.undeadWhite))
                     Text("Weekly")
-                    .foregroundColor(Color(UIColor.sicklySmurfBlue))
+                        .foregroundColor(Color(UIColor.sicklySmurfBlue))
                 }
-
+                
                 HStack (spacing: 16) {
-                    BarView(value: dataPoints[0])
-                    BarView(value: dataPoints[1])
-                    BarView(value: dataPoints[2])
-                    BarView(value: dataPoints[3])
-                    BarView(value: dataPoints[4])
-                    BarView(value: dataPoints[5])
-                    BarView(value: dataPoints[6])
-
+                    BarView(value: day7)
+                    BarView(value: day6)
+                    BarView(value: day5)
+                    BarView(value: day4)
+                    BarView(value: day3)
+                    BarView(value: day2)
+                    BarView(value: day1)
+                    
                 }.padding(.top, 0)
                     .animation(.default)
             }
@@ -44,8 +57,7 @@ struct ChartsView: View {
         }
     }
 }
-
-
+// MARK: - BarView
 struct BarView: View {
     var value: CGFloat = 0
     
@@ -58,7 +70,7 @@ struct BarView: View {
                     .foregroundColor(Color(UIColor.sicklySmurfBlue))
             }
             Text("D").padding(.top, 0)
-            .foregroundColor(Color(UIColor.undeadWhite))
+                .foregroundColor(Color(UIColor.undeadWhite))
             
         }
     }
@@ -67,5 +79,60 @@ struct BarView: View {
 struct ChartsView_Previews: PreviewProvider {
     static var previews: some View {
         ChartsView()
+    }
+}
+
+// MARK: - Extension
+extension ChartsView {
+    fileprivate mutating func checkIfIndexExists() {
+        if ((0 < lastSevenDays.count ? lastSevenDays[0] : nil) != nil) {
+            day1 = CGFloat(lastSevenDays[0].totalIntakeAmount)
+        }
+        
+        if ((1 < lastSevenDays.count ? lastSevenDays[1] : nil) != nil) {
+            day2 = CGFloat(lastSevenDays[1].totalIntakeAmount)
+        }
+        
+        if ((2 < lastSevenDays.count ? lastSevenDays[2] : nil) != nil) {
+            day3 = CGFloat(lastSevenDays[2].totalIntakeAmount)
+        }
+        
+        if ((3 < lastSevenDays.count ? lastSevenDays[3] : nil) != nil) {
+            day4 = CGFloat(lastSevenDays[3].totalIntakeAmount)
+        }
+        
+        if ((4 < lastSevenDays.count ? lastSevenDays[4] : nil) != nil) {
+            day5 = CGFloat(lastSevenDays[4].totalIntakeAmount)
+        }
+        
+        if ((5 < lastSevenDays.count ? lastSevenDays[5] : nil) != nil) {
+            day6 = CGFloat(lastSevenDays[5].totalIntakeAmount)
+        }
+        
+        if ((6 < lastSevenDays.count ? lastSevenDays[6] : nil) != nil) {
+            day7 = CGFloat(lastSevenDays[6].totalIntakeAmount)
+        }
+    }
+    
+    fileprivate mutating func updateDailyLogs() {
+        let allIntakeEntries = dailyLogController.allIntakeEntries
+        
+        let allDates = allIntakeEntries.compactMap { $0.timestamp?.startOfDay } // removes .hour, .minute, .second, etc. granularity from timestamps
+        let daysWithIntakeEntries = Array(Set(allDates)) // removes duplicates (each day appears only once)
+        
+        var dailyLogs: [DailyLog] = []
+        
+        for day in daysWithIntakeEntries {
+            let entries = allIntakeEntries.filter { $0.timestamp?.startOfDay == day }
+            dailyLogs.append(DailyLog(date: day, entries: entries))
+        }
+        
+        self.dailyLogs = dailyLogs.sorted { $0.date > $1.date }
+        
+        let slice = dailyLogs.suffix(7)
+        let weekArray = Array(slice)
+        lastSevenDays = weekArray
+        print(weekArray)
+        
     }
 }
