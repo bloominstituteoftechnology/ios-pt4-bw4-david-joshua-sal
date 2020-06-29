e00ffb564fe1fa01928a2427f2d4aeaf5f153ec7
David Wright <davidtwright89@gmail.com>
2020-06-29 02:54:47 -0400

    Implemented the accessor functions to the entries array in DailyLog.m

--- Hydrate/Hydrate/Models/DailyLog.m
+++ Hydrate/Hydrate/Models/DailyLog.m
@@ -8,6 +8,12 @@
 
 #import "DailyLog.h"
 
+@interface DailyLog ()
+
+@property (nonatomic) NSMutableArray <IntakeEntry *> *internalEntries;
+
+@end
+
 @implementation DailyLog
 
 - (instancetype)initWithDate:(NSDate *)date entries:(NSArray<IntakeEntry *> *)entries
@@ -18,15 +24,45 @@
                                                              NSCalendarUnitMonth |
                                                              NSCalendarUnitDay) fromDate:date];
         _date = [calendar dateFromComponents:components];
-        _entries = entries.copy;
+        _internalEntries = entries.mutableCopy;
     }
     return self;
 }
 
 - (NSInteger)totalIntakeAmount
 {
-    NSNumber *sum = [_entries.copy valueForKeyPath:@"@sum.intakeAmount"];
+    NSNumber *sum = [self.internalEntries.copy valueForKeyPath:@"@sum.intakeAmount"];
     return sum.integerValue;
+}
+
+- (NSInteger)entryCount
+{
+    return self.internalEntries.count;
+}
+
+- (NSArray<IntakeEntry *> *)entries
+{
+    return self.internalEntries.copy;
+}
+
+- (IntakeEntry *)entryAtIndex:(NSInteger)index
+{
+    return [self.internalEntries objectAtIndex:index];
+}
+
+- (void)addEntry:(IntakeEntry *)entry
+{
+    [self.internalEntries addObject:entry];
+}
+
+- (void)updateEntryAtIndex:(NSInteger)index withEntry:(nonnull IntakeEntry *)entry
+{
+    [self.internalEntries replaceObjectAtIndex:index withObject:entry];
+}
+
+- (void)removeEntry:(IntakeEntry *)entry
+{
+    [self.internalEntries removeObject:entry];
 }
 
 @end
