ad94f92d3054b6f22e5c4a8f347b3281f4c448fc
Sal B Amer <iosdevsalman@gmail.com>
2020-06-30 08:27:50 -0700

    Finished graphicCircular
    Finished graphicCircular Complication

--- Hydrate/Hydrate-WatchOS Extension/ComplicationController.swift
+++ Hydrate/Hydrate-WatchOS Extension/ComplicationController.swift
@@ -39,7 +39,25 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         let volInMilliliters = Measurement(value: currentWaterVolume, unit: UnitVolume.milliliters)
         
         let displayVolume: String
-        let prefferedUnit == prefferedUnit
+        let prefferedUnit = PreferredUnit(rawValue: UserDefaults.standard.integer(forKey: "Hydrate_preferredUnit"))
+        
+        if prefferedUnit == PreferredUnit.fluidOunces {
+            displayVolume = String(format: "%.0f oz", volInMilliliters.converted(to: .fluidOunces).value)
+        } else {
+            displayVolume = String(format: "%.1f L", volInMilliliters.converted(to: .liters).value)
+        }
+        
+        //switch view depending on complication family
+        switch complication.family {
+        case .graphicCircular:
+            let template = CLKComplicationTemplateGraphicCircularClosedGaugeText()
+            template.centerTextProvider = CLKSimpleTextProvider(text: "💧")
+            template.gaugeProvider = CLKSimpleGaugeProvider(style: .fill, gaugeColor: #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1), fillFraction: fillFraction)
+            let entry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+            handler(entry)
+        default:
+            handler(nil)
+        }
         
     }
     
