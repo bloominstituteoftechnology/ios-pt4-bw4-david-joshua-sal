06faa823fc7aa70bf95562047d4dc1a75b8803a6
Sal B Amer <iosdevsalman@gmail.com>
2020-06-30 08:17:06 -0700

    Added PreferredUnit Enum
    Added PreferredUnit Enum

--- Hydrate/Hydrate-WatchOS Extension/ComplicationController.swift
+++ Hydrate/Hydrate-WatchOS Extension/ComplicationController.swift
@@ -10,13 +10,37 @@ import WatchKit
 import ClockKit
 import UIKit
 
+enum PreferredUnit: Int {
+    case milliliters
+    case fluidOunces
+}
+
 class ComplicationController: NSObject, CLKComplicationDataSource {
+    
+    
+    
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        <#code#>
+        handler([])
     }
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        <#code#>
+        
+        var currentWaterVolume: Double = 0.0
+        var percentageOfDailyGoal: Double = 0.0
+        
+        let lastUpdated = UserDefaults.standard.value(forKey: "Hydrate_lastUpdated") as? Date ?? Date.distantFuture
+        if Calendar.current.isDateInToday(lastUpdated) {
+            currentWaterVolume = UserDefaults.standard.double(forKey: "Hydrate_currentWaterVolume")
+            percentageOfDailyGoal = UserDefaults.standard.double(forKey: "Hydrate_percentageOfDailyGoal")
+        }
+        
+        let fillFraction = Float(min(1.0, percentageOfDailyGoal))
+        let displayPercentOfGoal = String(format: "%.0f%%", percentageOfDailyGoal * 100)
+        let volInMilliliters = Measurement(value: currentWaterVolume, unit: UnitVolume.milliliters)
+        
+        let displayVolume: String
+        let prefferedUnit == prefferedUnit
+        
     }
     
 
